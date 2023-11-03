9ff505cb34052265757f0ebe9c61f802f23acc9d
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2023-11-03 18:23:22 +0300

    Added image dto and a mapper for it

---
+++ Server/Extensions/DataMappers.cs
@@ -0,0 +1,26 @@
+﻿using Trofi.io.Shared;
+
+namespace Trofi.io.Server;
+
+/// <summary>
+/// This class provides extensions for all the DTOs in the project so that
+/// domain models can be mapped to DTOs more easily
+/// </summary>
+public static class DataMappers
+{
+    #region Image Mappers
+    public static ImageDto ToDishImageDto(this DishImage dishImage)
+    {
+        return new ImageDto
+        {
+            Id = dishImage.Id,
+            URL = dishImage.URL
+        };
+    }
+    #endregion
+}
+
+public static class ModelMappers
+{
+
+}
---
+++ Shared/DTOs/ImageDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Trofi.io.Shared;
+
+public class ImageDto
+{
+    public Guid Id { get; set; }
+    public string? URL { get; set; }
+}
