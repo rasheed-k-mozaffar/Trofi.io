b1766b941df206584aaed3a9613b1b134fbaa779
Rasheed <rasheedkmozaffar@hotmail.com>
2023-11-03 19:08:17 +0300

    Implemented the files repository

---
+++ Server/Repositories/IFilesRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Trofi.io.Server.Repositories;
+
+public interface IFilesRepository
+{
+    Task AddDishImageAsync(string imageUrl, Guid dishId);
+    Task<DishImage> GetDishImageAsync(Guid imageId);
+    Task DeleteDishImageAsync(Guid imageId);
+}
