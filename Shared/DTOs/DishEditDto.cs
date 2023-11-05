80a72db5fd7507504f4e30f909b6a61e1f5ade2c
Rasheed <rasheedkmozaffar@hotmail.com>
2023-11-05 18:41:25 +0300

    Created and mapped the required DTOs for the menu

---
+++ Shared/DTOs/DishEditDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Trofi.io.Shared;
+
+public class DishEditDto
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public double Price { get; set; }
+    public double? UpdatedPrice { get; set; }
+    public bool IsAvailable { get; set; }
+    public bool IsSpecial { get; set; }
+}
