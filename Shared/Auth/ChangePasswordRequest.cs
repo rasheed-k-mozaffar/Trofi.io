ef84c5eaef1bc27c41775688d44ec8faefa1a2d7
Rasheed <rasheedkmozaffar@hotmail.com>
2023-11-06 18:43:08 +0300

    Added data validations to the auth requests

--- Shared/Auth/ChangePasswordRequest.cs
+++ Shared/Auth/ChangePasswordRequest.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Trofi.io.Shared.Auth
 {
     public class ChangePasswordRequest
     {
-        public string email { get; set; } = string.Empty;
-        public string oldPassword { get; set; } = string.Empty;
-        public string newPassword { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Your email is required")]
+        [EmailAddress(ErrorMessage = "The email you entered is an invalid email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Your old password is required")]
+        public string? OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Your new password is required")]
+        public string? NewPassword { get; set; }
     }
 }
