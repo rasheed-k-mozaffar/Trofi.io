1115097a2051d9d0630285ebb6781ab643e42de1
Rasheed <rasheedkmozaffar@hotmail.com>
2023-11-06 18:48:00 +0300

    Added required classes for refresh tokens

---
+++ Server/Models/UserManagerResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Trofi.io.Server.Models;
+
+public class UserManagerResponse
+{
+    public string? Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public string? JWT { get; set; }
+    public string? RefreshToken { get; set; }
+    public DateTime RefreshTokenExpiration { get; set; }
+}
+
