5c24eb131a874b16fb8cd4ec2931c45c87575f0b
Rasheed <rasheedkmozaffar@hotmail.com>
2023-11-09 14:08:27 +0300

    Added a custom service to access user info

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.ResponseCompression;
 using Trofi.io.Server.Extensions;
+using Trofi.io.Server.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,28 @@ builder.Services.AddRazorPages();
 builder.Services.AddCustomServicesToDiContainer();
 
 builder.Services.RegisterDbContextAndIdentity(builder.Configuration);
+
+builder.Services.AddBearerAndConfigureAuthentication(builder.Configuration);
+
+// This service provides access to the current authenticated user's information
+// including the user's id and their cart id
+builder.Services.AddScoped(sp =>
+{
+    var userInfo = new UserInfo();
+
+    var httpContextAccessor = sp.GetService<IHttpContextAccessor>();
+    if (httpContextAccessor is not null)
+    {
+        var httpContext = httpContextAccessor.HttpContext;
+        if (httpContext is not null && httpContext.User.Identity!.IsAuthenticated)
+        {
+            var user = httpContext.User;
+            userInfo.UserId = user.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+            userInfo.CartId = Guid.Parse(user.FindFirst("CartId")!.Value);
+        }
+    }
+    return userInfo;
+});
 #endregion
 
 var app = builder.Build();
@@ -28,6 +51,9 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
