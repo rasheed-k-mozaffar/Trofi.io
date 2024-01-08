7daca28e2c6b0c25ad74b07ea767899b0b0f7080
Rasheed Mozaffar <rasheedkmozaffar@hotmail.com>
2024-01-08 18:18:29 +0300

    Added categories table to the database

---
+++ Server/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trofi.io.Server.Models;
+
+public class Category
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [StringLength(maximumLength: 50)]
+    public string? Name { get; set; }
+
+    [StringLength(maximumLength: 250)]
+    public string? Description { get; set; }
+
+    public string? LogoUrl { get; set; } // this property will be used to store the logo or image of a category
+
+    public virtual ICollection<MenuItem>? Items { get; set; } = new List<MenuItem>();
+}
